102b1cfbdf16e4c2198f0664a890f19926f4b5a8
Mike Ryan <mike.ryan52@gmail.com>
2015-04-20 16:17:07 -0500

    Fixing game over screen

--- views/game-over.lua
+++ views/game-over.lua
@@ -2,12 +2,13 @@ local composer = require 'composer'
 local scene = composer.newScene()
 
 function scene:create(event)
-	print('Game over')
+	local sceneGroup = self.view
 	local gameover = display.newRoundedRect( display.contentCenterX, display.contentCenterY, 300, 150, 5)
 	local gameovertext = display.newText( "GAME OVER", display.contentCenterX, display.contentCenterY, system.NativeFont, 50 )
 	gameovertext:setFillColor( 0,0,0 )
-
-	gameovertext:addEventListener('tap', function()
+	sceneGroup:insert(gameover)
+	sceneGroup:insert(gameovertext)
+	gameover:addEventListener('tap', function()
 		composer.gotoScene('menu.menu')
 	end)
 end
